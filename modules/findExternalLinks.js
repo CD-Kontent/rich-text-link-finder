6c45bd41714124646f8c5240cbdc771fc6a30930
Champ-Dog <132868828+CD-Kontent@users.noreply.github.com>
2024-04-12 12:31:58 +1000

    saving progress

--- modules/findExternalLinks.js
+++ modules/findExternalLinks.js
@@ -1,19 +1,28 @@
+import handleLink from "./checkLinks.js";
+
 // Compares a string against a regex pattern to find URLs
-function extractURLs(text) {
+function findLinks(text) {
   const regex = /<a([^>]+)href="(https?:\/\/.+?)"[^>]*>(.+?)<\/a>/g;
   return text.match(regex);
 }
 
-function findExternalLinks(contentItems) {
+// Refactor to separate functions
+// Also I don't need to send content item name and id, it's unnecessarily increasing payloa
+async function findExternalLinks(contentItems) {
   contentItems.forEach((item) => {
-    const externalLinks = [];
+    // const externalLinks = [];
     let contentElements = Object.entries(item.elements);
     contentElements.forEach((element) => {
       if (element[1].type === "rich_text") {
-        const foundLinks = extractURLs(element[1].value);
+        const foundLinks = findLinks(element[1].value);
         if (foundLinks !== null) {
           foundLinks.forEach((link) => {
-            doTheThing(link);
+            const url = link.split('"')[1];
+            handleLink({
+              name: item.system.name,
+              id: item.system.id,
+              url: url,
+            });
           });
         }
       }
@@ -21,8 +30,5 @@ function findExternalLinks(contentItems) {
   });
 }
 
-function doTheThing(URL) {
-  console.log(URL);
-}
 
-export default(findExternalLinks)
+export default findExternalLinks;
